53e7cd76807dc6700721c99b608eb667f58c9837
Deepak sai challa <u23ai080@coed.svnit.ac.in>
2025-10-01 01:23:54 +0530

    Lab-4,5

--- Lab3/top.py
+++ Lab3/top.py
@@ -8,15 +8,12 @@ def count_top_tokens(input_file, output_file, top_n=500):
         for line in f:
             tokens = line.strip().split()
             
-            # Ignore token if it is only "."
             tokens = [tok for tok in tokens if tok != "."]
 
             token_counter.update(tokens)
 
-    # Get top N most common tokens
     top_tokens = dict(token_counter.most_common(top_n))
 
-    # Save to JSON file (keep Telugu/Hindi readable)
     with open(output_file, "w", encoding="utf-8") as out:
         json.dump(top_tokens, out, ensure_ascii=False, indent=2)
 
